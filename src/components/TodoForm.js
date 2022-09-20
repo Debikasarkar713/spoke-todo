eda3745d7586ad1f210d837a35e068ad997a557b
Debika Sarkar <debikasarkar713@gmail.com>
2022-09-20 12:31:01 -0400

    Add checkbox and divs for starting of task placement

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,6 +3,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { setTaskSlice } from "../redux/slice/task";
 import { nanoid } from "@reduxjs/toolkit";
 import { CREATE_TASK, UPDATE_TASK_BY_ID } from "../redux/types";
+import styled from "styled-components";
+
+const FormWrapper = styled.section`
+  justify-content: center;
+`;
 
 const TodoForm = () => {
   const task = useSelector((state) => state.task);
@@ -23,33 +28,35 @@ const TodoForm = () => {
     );
   };
   return (
-    <form>
-      {/* <label>
-        Task Number:
-        <input value={task.id} placeholder="title" type="text" />
-      </label> */}
-      <label>
-        Title:
-        <input
-          onChange={handleChange("title")}
-          placeholder="title"
-          type="text"
-          value={task.title}
-          name="title"
-        />
-      </label>
-      <label>
-        Task:
-        <input
-          onChange={handleChange("message")}
-          placeholder="enter here"
-          type="text"
-          value={task.message}
-          name="message"
-        />
-      </label>
-      <input type="button" onClick={() => handleSubmit()} value="Submit" />
-    </form>
+    <FormWrapper>
+      <form>
+        <label>
+          Task Number:
+          <input value={task.id} placeholder="title" type="text" />
+        </label>
+        <label>
+          Title:
+          <input
+            onChange={handleChange("title")}
+            placeholder="title"
+            type="text"
+            value={task.title}
+            name="title"
+          />
+        </label>
+        <label>
+          Task:
+          <input
+            onChange={handleChange("message")}
+            placeholder="enter here"
+            type="text"
+            value={task.message}
+            name="message"
+          />
+        </label>
+        <input type="button" onClick={() => handleSubmit()} value="Submit" />
+      </form>
+    </FormWrapper>
   );
 };
 export default TodoForm;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,35 +4,64 @@ import { setTaskSlice } from "../redux/slice/task";
 import { GET_TASKS, DELETE_TASK_BY_ID } from "../redux/types";
 import styled from "styled-components";
 
-const TodoWrapper = styled.div`
+const TodoWrapper = styled.section`
   display: flex;
   border: 1px solid red;
-  font-size: 12px;
+  font-size: 20px;
 `;
+const CompleteDiv = styled.div`
+  display: block;
+  border: 1px solid green;
+  padding: 20px;
+`;
+
+const TaskDiv = styled.div`
+  display: block;
+  border: 1px dotted purple;
+  padding: 10px;
+`;
+
 export default function TodoList() {
   const rows = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
   React.useEffect(() => dispatch({ type: GET_TASKS }), []);
   return (
     <TodoWrapper>
-      <ul>
-        {rows.map((row) => (
-          <div key={row.id}>
-            <ul>
-              <li>Task Number: {row.id}</li>
-              <li>Title: {row.title}</li>
-              <li>Details: {row.message}</li>
-            </ul>
-            <input type="checkbox" />
-            <button onClick={() => dispatch(setTaskSlice(row))}>EDIT</button>
-            <button
-              onClick={() => dispatch({ type: DELETE_TASK_BY_ID, id: row.id })}
-            >
-              DELETE
-            </button>
-          </div>
-        ))}
-      </ul>
+      <CompleteDiv>
+        INCOMPLETE TASKS
+        <TaskDiv>
+          <ul>
+            {rows.map((row) => (
+              <div key={row.id}>
+                <ul>
+                  <li>Task Number: {row.id}</li>
+                  <li>Title: {row.title}</li>
+                  <li>Details: {row.message}</li>
+                </ul>
+
+                <button onClick={() => dispatch(setTaskSlice(row))}>
+                  EDIT
+                </button>
+                <button
+                  onClick={() =>
+                    dispatch({ type: DELETE_TASK_BY_ID, id: row.id })
+                  }
+                >
+                  DELETE
+                </button>
+                <div>
+                  <label>
+                    <input type="checkbox" />
+                    Complete
+                  </label>
+                </div>
+              </div>
+            ))}
+          </ul>
+        </TaskDiv>
+      </CompleteDiv>
+      <CompleteDiv>COMPLETED TASKS</CompleteDiv>
+      <br />
     </TodoWrapper>
   );
 }
